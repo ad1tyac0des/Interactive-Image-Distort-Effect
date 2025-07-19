f1a0d1d332185d3470c01a01620242327b224ea2
Aditya Singh <adityasingh740745@gmail.com>
2025-07-19 09:26:52 +0530

    fix: update logoPath to use relative path

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { vertexShader, fragmentShader } from "./shaders.js";
 
 const config = {
-    logoPath: "logo.png",
+    logoPath: "./logo.png",
     logoSize: 1250,
     logoColor: "#404040",
     canvasBg: "#141414",
